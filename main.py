e1d8619c556cdef3a6849b4a089cd3e0d0172935
Anh Tu <sweantu@gmail.com>
2025-03-13 07:43:46 +0700

    feat: tutorial

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+import dlt
+from dlt.sources.helpers.rest_client import RESTClient
+from dlt.sources.helpers.rest_client.paginators import PageNumberPaginator
+
+
+@dlt.resource(name="rides", write_disposition="replace")
+def ny_taxi():
+    client = RESTClient(
+        base_url="https://us-central1-dlthub-analytics.cloudfunctions.net",
+        paginator=PageNumberPaginator(base_page=1, total_path=None),
+    )
+
+    for page in client.paginate("data_engineering_zoomcamp_api"):
+        yield page
+
+
+pipeline = dlt.pipeline(
+    pipeline_name="fs_pipeline",
+    destination="filesystem",  # <--- change destination to 'filesystem'
+    dataset_name="fs_data",
+)
+
+load_info = pipeline.run(
+    ny_taxi, loader_file_format="parquet"
+)  # <--- choose a file format: parquet, csv or jsonl
+print(load_info)
